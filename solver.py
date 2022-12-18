80750c71e867f97b951859a96caa561e4ece065e
IgneousPotato <tase_anish@yahoo.co.in>
2022-12-18 18:59:29 +0000

    Changed function to get domains

--- solver.py
+++ solver.py
@@ -13,7 +13,7 @@ class Solver:
     
     def __init__(self, board, words) -> None:
         self.size = board.size
-        self.letters = list(tile.letter for tile in board.tiles)
+        self.letters = list(set(tile.letter for tile in board.tiles))
         self.board = np.ndarray((self.size, self.size), dtype=list)
         self.mask = np.ndarray((self.size, self.size), dtype=list)
         
@@ -43,37 +43,25 @@ class Solver:
                     word = temp_board[:, j*2]
                     mask = temp_mask[:, j*2]
 
-                poss_word_dict[i*3 + j] = self.poss_words(word, mask, letters)
-
-        # print(poss_word_dict)
-        # orderedDict = sorted(poss_word_dict.items(), key=lambda e: e[1][0])
-        # print(orderedDict)
-
-        # logging.info("Possible words dictionary")
-        # for i in orderedDict:
-        #     print(i)
+                poss_word_dict[i*3 + j] = self.get_domain(word, mask)
             
 
-    def poss_words(self, word, mask, letters):
-        """
-        I HATE THIS - I NEED TO REDO THIS LATER. IT WAS BRAIN THOUGHT VOMIT, OKAY? 
-        I JUST NEEDED IT TO WORK FIRST. IMPROVEMENTS CAN COME LATER.
-        THANKS. BYE.
-        """
-
-        r_l = letters
+    def get_domain(self, word, mask):
         g_letters = ''
         y_letters = ''
+        y_idx = []
+        empty_idx = []
         score = 0
 
-        for count, char in enumerate(mask):
-            if char == 1:
+        for count, current_char in enumerate(mask):
+            if current_char == 1:
                 g_letters += word[count]
                 y_letters += '.'            
                 score += 5
-            elif char == 0:
+            elif current_char == 0:
                 g_letters += '.'
                 y_letters += word[count]
+                y_idx.append(count)
                 if count % 2 == 0:
                     score += 1
                 else:
@@ -81,83 +69,43 @@ class Solver:
             else:
                 g_letters += '.'
                 y_letters += '.'
+                empty_idx.append(count)
 
-        pattern = re.compile(g_letters, re.IGNORECASE)
-        g_mask_domain = [x for x in self.english_words if re.match(pattern, x)]
-
-        logging.info(f'G Mask: {g_letters}')
-        print('G Domain:')
-        print(f'{g_mask_domain}\n')
-        
-        if y_letters != '.....':
-            y_odd_domain = []
-            y_even_domain = []
-
-            ymd = g_mask_domain
-
-            for count, char in enumerate(y_letters): 
-                yeven = ymd
-
-                if char != '.':
-                    if count % 2 == 0:
-                        y_mask_domain = yeven
-                    else: 
-                        y_mask_domain = ymd
-
-                    # Get mask for single yellow char
-                    y_list = list('....')
-                    y_list.insert(count, char)
-                    y_mask = ''.join(y_list)
-                    
-                    pattern = re.compile(char, re.IGNORECASE)
-                    y_mask_domain = [x for x in y_mask_domain if re.search(pattern, x)]
-
-                    pattern = re.compile(y_mask, re.IGNORECASE)           
-                    y_mask_domain = [x for x in y_mask_domain if not re.match(pattern, x)]
-
-                    if g_letters != '.....':
-                        for g_count, g_char in enumerate(g_letters):
-                            new_y_list = list('....')
-
-                            if g_char != '.' and g_char != char:
-                                new_y_list.insert(g_count, char)
-                                new_y_mask = ''.join(new_y_list)
-
-                                pattern = re.compile(new_y_mask, re.IGNORECASE)           
-                                y_mask_domain = [x for x in y_mask_domain if not re.match(pattern, x)]
-
-                    if count % 2 == 0:
-                        yeven = y_mask_domain
-                        # y_even_domain.append(y_mask_domain)
-                    else:
-                        ymd = y_mask_domain
-                        # y_odd_domain.append(y_mask_domain)
-
-            '''print(y_odd_domain)
-            word_domain = list(set.intersection(*map(set, [y_odd_domain])))
-            if word_domain != []:
-                word_domain_ext = list(set.intersection(*map(set, [word_domain, y_even_domain])))
-            else:
-                print(y_even_domain)
-                word_domain_ext = list(set.intersection([y_even_domain]))'''
-            word_domain = ymd
-            word_domain_ext = yeven
+        if g_letters != '.....':
+            pattern = re.compile(g_letters, re.IGNORECASE)
+            g_mask_domain = [x for x in self.english_words if re.match(pattern, x)]
         else:
-            logging.info('No Y mask\n')
+            g_mask_domain = self.english_words
+            
+        word_domain = []
+        if y_letters[1::2] != '..': 
+            count = 1
+
+            while count < self.size: 
+                current_char = y_letters[count].lower()
+                
+                if current_char != '.':            
+                    temp_y_idx = y_idx
+                    temp_y_idx.remove(count)
+                    y_mask = empty_idx + temp_y_idx
+
+                    pattern = re.compile(current_char, re.IGNORECASE)
+                    words_with_y = [x for x in g_mask_domain if re.search(pattern, x)]
+
+                    for word in words_with_y:
+                        if current_char in [word[i] for i in y_mask]:
+                            word_domain.append(word) 
+                count += 2
+        else:
             word_domain = g_mask_domain
-            word_domain_ext = g_mask_domain
-        
+
+        print()
         logging.info(f'Green Letters: {list(g_letters)}')
         logging.info(f'Yellow Letters: {list(y_letters)}')
         logging.info(f'Score: {score}\n')
+        logging.info(f'Word domain:')
+        print(word_domain)
+        print()
 
-        logging.info(f'Word Domain: ')
-        print(f'{word_domain}\n')
-
-        logging.info(f'Word Domain Extended: ')
-        print(f'{word_domain_ext}\n')
-
-        print('\n===============================================================================================================================================')
-        print('===============================================================================================================================================\n')
-
-        return word_domain, word_domain_ext
+        return word_domain, score
+        
